6815243540ee515a507e19e14c9b0961e6d7bf26
qwleha <qwleha@yandex.ru>
2020-01-17 18:45:40 +0300

    загрузил файлы

--- classifier/settings.py
+++ classifier/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'classifier.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.ostgresql_psycorpg2',
+        'NAME': 'articles',
+		'USER': 'articles_admin',
+		'PASSWORD': 'mypass',
+		'HOST': '127.0.0.1',
+		'PORT': '5432',
     }
 }
 
